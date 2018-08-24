aabed96417f6ec9d18baf6721ad1b95c41d53e4d
micaela.martino <micaela.martino@despegar.com>
2018-08-24 01:30:40 -0300

    Update configs and repos

--- definitions.sh
+++ definitions.sh
@@ -5,18 +5,15 @@ fcp() {
 	mkdir -p "$2" && \cp -rf "$1" "${2%/}/$3"; 
 }
 
-check_root() { 
-	if [ "$EUID" -ne 0 ]; then echo "Please run as root."; exit; fi; 
-}
-
-add_repositories() {
+add_respositories() {
 	add-apt-repository -y ppa:aacebedo/libi3ipc-glib
-	add-apt-repository -y ppa:aacebedo/xfce4-i3-workspaces-plugin
-	apt-get update
+	#add-apt-repository -y ppa:aacebedo/xfce4-i3-workspaces-plugin
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E9B91A3661D4724D5CAA5E2B21ACA1DAE834F228
+  echo "deb http://ppa.launchpad.net/aacebedo/xfce4-i3-workspaces-plugin/ubuntu artful main" | sudo tee /etc/apt/sources.list.d/xfce4-i3-workspaces.list
+  apt update
 }
 
 install_all() {
-	apt-get install -y gnome-disk-utility
 	apt-get install -y gmrun
 	apt-get install -y nitrogen
 	apt-get install -y i3
@@ -25,18 +22,20 @@ install_all() {
 }
 
 setup_i3() {
-  i3_path=$INIT_ENV_DIR/conf/i3
-	fcp $i3_path/i3.desktop $XFCE_AUTOSTART_PATH
-	fcp $i3_path/. $I3_CONFIG_PATH
+	cd i3/
+	fcp i3.desktop $XFCE_AUTOSTART_PATH
+	#fcp config $I3_CONFIG_PATH
+	cd ..
 }
 
 setup_xfce() {
   xfce_path=$INIT_ENV_DIR/conf/xfce
-	fcp $xfce_path/xfce4-keyboard-shortcuts.xml $XFCE_CONFIG_PATH
-	fcp $xfce_path/xfce4-session-host:0 $XFCE_SESSIONS_PATH xfce4-session-$HOSTNAME:0
+  fcp $xfce_path/xfce4-keyboard-shortcuts.xml $XFCE_CONFIG_PATH
 }
 
 setup_all() {
+	cd conf/
 	setup_i3
-	setup_xfce
+  setup_xfce
+	cd ..
 }
--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ CONTAINING_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 source "$CONTAINING_DIR/definitions.sh"
 cd $CONTAINING_DIR
 
-check_root
+if [ "$EUID" -ne 0 ]; then echo "Please run as root."; exit; fi;
+
 add_repositories
 install_all
 setup_all
